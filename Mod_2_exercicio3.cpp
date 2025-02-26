b3e443026e747ea40ca58da259d16b37119602f2
Gio1807 <geovannihenriquedelima@hotmail.com>
2025-02-26 16:37:49 -0300

    Exercicio 3

---
+++ Mod_2_exercicio3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+ 
+    /* entrada
+    inserir distancia percorrida e quantidade de litros gastos */
+
+    float distancia_percorrida;
+    int litros_gasolina;
+
+
+    cout <<"digite a distancia total percorrida";
+    cin >> distancia_percorrida;
+    cout << "digite a quantidade de litros gastos"; 
+    cin >> litros_gasolina;
+
+
+    /* Processamento 
+    calcular a média do que o usuário gastou */ 
+
+    float divisao;
+
+    divisao = distancia_percorrida / litros_gasolina;
+
+    /*Saída
+    Mostrar o valor médio gasto*/
+
+    cout: "o valor médio que você gastou neste percursso foi de: " << divisao = distancia_percorrida / litros_gasolina;
+
+    
+
+
+
+}
